d971f3e63a0b897bacba2612c1ec6859f57436f4
SurajAiri <surajairi.ml@gmail.com>
2025-07-15 17:09:36 +0530

    cors-open-4all

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(responseFormatter);
 
